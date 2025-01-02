1f96079fe455bc756345a2339d0512c5906ad984
liamcharger <chargerelectronics19@gmail.com>
2025-01-01 21:52:10 -0500

    Initial Commit

---
+++ OpenID/OpenIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenIDApp.swift
+//  OpenID
+//
+//  Created by Liam Willey on 1/1/25.
+//
+
+import SwiftUI
+
+@main
+struct OpenIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
